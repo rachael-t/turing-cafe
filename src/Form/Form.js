f85f2828f0187ccb61b701b4a02c0315349debfd
Rachael Thomas <rnthomas001@gmail.com>
2020-05-21 09:24:50 -0600

    Setup the Form class component

---
+++ src/Form/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <h2>Hello</h2>
+
+        )
+    }
+}
+
+export default Form;
