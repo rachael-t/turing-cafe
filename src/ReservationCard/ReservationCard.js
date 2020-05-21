d79f8c78d941ab6724b5991797da0964b10ac373
Rachael Thomas <rnthomas001@gmail.com>
2020-05-21 09:51:14 -0600

    Add JSX so that fetched reservation data displays on the DOM

--- src/ReservationCard/ReservationCard.js
+++ src/ReservationCard/ReservationCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './ReservationCard.css';
 
-const ReservationCard = ({ reservations }) => {
+const ReservationCard = ({ date, id, name, number, time }) => {
     return (
         <div className='reservation-card'>
-        
+            <h3>{name}</h3>
+            <p>{date}</p>
+            <p>{time} pm</p>
+            <p>Number of guests: {number}</p>
+            <button className={id}>
+                Cancel
+            </button>
         </div>
     )
 }
--- src/ReservationContainer/ReservationContainer.js
+++ src/ReservationContainer/ReservationContainer.js
@@ -3,9 +3,14 @@ import './ReservationContainer.css';
 import ReservationCard from '../ReservationCard/ReservationCard';
 
 const ReservationContainer = ({ reservations }) => {
+    const reservationsToRender = reservations.map(reservation => {
+        return <ReservationCard { ...reservation } />
+    })
+
     return (
         <div className='reservation-container'>
             <h2>Current Reservations:</h2>
+            {reservationsToRender}
         </div>
     )
 }
