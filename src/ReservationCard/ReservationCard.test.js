24536cc90612131f2f5a73419b40ac5cbe0f5648
Rachael Thomas <rnthomas001@gmail.com>
2020-05-21 10:53:22 -0600

    Create and pass test for rendering of reservation card details

---
+++ src/ReservationCard/ReservationCard.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReservationCard from './ReservationCard';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('ReservationCard', () => {
+    it('should render a reservation card', () => {
+        const { getByText } = render(<ReservationCard 
+            date={'12/29'}
+            id={1}
+            name={'Christie'}
+            number={12}
+            time={'7:00'}
+        />)
+
+        const resName = getByText('Christie');
+        const resDate = getByText('12/29');
+        const resTime = getByText('7:00 pm');
+        const resGuests = getByText('Number of guests: 12');
+        const cancelBtn = getByText('Cancel');
+
+        expect(resName).toBeInTheDocument();
+        expect(resDate).toBeInTheDocument();
+        expect(resTime).toBeInTheDocument();
+        expect(resGuests).toBeInTheDocument();
+        expect(cancelBtn).toBeInTheDocument();
+    })
+
+})
