598347ff46f69da415d74f82a436ea92f5b0114f
Rachael Thomas <rnthomas001@gmail.com>
2020-05-21 09:33:53 -0600

    Setup form, input, and button JSX as well as declare handleChange and submitReservation functions

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -5,14 +5,57 @@ class Form extends Component {
     constructor() {
         super();
         this.state = {
-
+            id: '',
+            name: '',
+            date: '',
+            time: '',
+            number: '',
         }
+    }
+
+    handleChange = (e) => {
+
+    }
+
+    submitReservation = (e) => {
+
     }
 
     render() {
         return (
-            <h2>Hello</h2>
-
+            <form>
+                <input
+                    type='text'
+                    placeholder='Name'
+                    name='name'
+                    value={this.state.name}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type='text'
+                    placeholder='Date (mm/dd)'
+                    name='date'
+                    value={this.state.date}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type='text'
+                    placeholder='Time'
+                    name='time'
+                    value={this.state.time}
+                    onChange={this.handleChange}
+                />
+                 <input
+                    type='text'
+                    placeholder='Number of guest'
+                    name='number'
+                    value={this.state.number}
+                    onChange={this.handleChange}
+                />
+                <button onClick={this.submitReservation}>
+                    Make Reservation
+                </button>
+            </form>
         )
     }
 }
