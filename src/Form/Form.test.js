e5db97787b8cab6cc5aa2c16d928fbd3db3bbe3c
Rachael Thomas <rnthomas001@gmail.com>
2020-05-21 11:03:08 -0600

    Write and pass a unit test for the form component that checks that when data is put into the form, the value is reflected in that form input

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -4,6 +4,7 @@ import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Form', () => {
+
     it('should render a form', () => {
         const { getByText, getByPlaceholderText } = render(<Form />)
 
@@ -18,6 +19,25 @@ describe('Form', () => {
         expect(timeInput).toBeInTheDocument();
         expect(numInput).toBeInTheDocument();
         expect(submitBtn).toBeInTheDocument();
+    });
+
+    it('should update the input value when data is entered', () => {
+        const { getByPlaceholderText } = render(<Form />)
+
+        const nameInput = getByPlaceholderText('Name');
+        const dateInput = getByPlaceholderText('Date (mm/dd)');
+        const timeInput = getByPlaceholderText('Time');
+        const numInput = getByPlaceholderText('Number of guests');
+
+        fireEvent.change(nameInput, { target: { value: 'Luna' }});
+        fireEvent.change(dateInput, { target: { value: '06/08' }});
+        fireEvent.change(timeInput, { target: { value: '6:30' }});
+        fireEvent.change(numInput, { target: { value: 4 }});
+
+        expect(nameInput.value).toBe('Luna');
+        expect(dateInput.value).toBe('06/08');
+        expect(timeInput.value).toBe('6:30');
+        expect(numInput.value).toBe('4');
     })
 
 })
