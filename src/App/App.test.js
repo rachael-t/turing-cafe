d1690a85f7dff7d7fb2ae05abb566f5404cb3354
Rachael Thomas <rnthomas001@gmail.com>
2020-05-21 11:13:58 -0600

    Write and pass an integration test for App that tests the user flow for adding a new reservation

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render, waitFor, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('should display a reservation on the page once a user submits it', () => {
+    const { getByText, getByPlaceholderText } = render(<App />);
+
+    const nameInput = getByPlaceholderText('Name');
+    const dateInput = getByPlaceholderText('Date (mm/dd)');
+    const timeInput = getByPlaceholderText('Time');
+    const numInput = getByPlaceholderText('Number of guests');
+
+    fireEvent.change(nameInput, { target: { value: 'Luna' }});
+    fireEvent.change(dateInput, { target: { value: '06/08' }});
+    fireEvent.change(timeInput, { target: { value: '6:30' }});
+    fireEvent.change(numInput, { target: { value: 4 }});
+    fireEvent.click(getByText('Make Reservation'));
+
+    expect(getByText('Number of guests: 4')).toBeInTheDocument();
+  })
+})
